be5c3ddf5454c9bf918560f3697da6bb8a6bb2c7
GuitarmonYz <guitarmonyz@gmail.com>
2017-11-18 17:42:21 -0500

    set up cube environment, finish shooting function

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GunController : MonoBehaviour {
+	public Rigidbody bullet;
+	public int bulletSpeed;
+	public Transform barrel;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void SpawnBullet() {
+		Rigidbody b;
+		b = Instantiate(bullet, new Vector3(barrel.position.x, barrel.position.y, barrel.position.z), barrel.rotation) as Rigidbody;
+		b.AddForce(b.transform.forward * bulletSpeed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// TODO: move make this more generic
+		if (Input.GetButtonDown("Fire1")) {
+			SpawnBullet();
+		}
+	}
+}
