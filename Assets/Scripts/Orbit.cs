854e434364aa8bcb67dcbe110bbdfd148171f3c9
GuitarmonYz <guitarmonyz@gmail.com>
2017-11-20 23:48:23 -0500

    add distorted glass effect, add enemy instances in scene

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+	public GameObject center;
+	public float speed;
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(center.transform.position, Vector3.right, speed*Time.deltaTime);
+	}
+}
